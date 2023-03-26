5a61db893a70de7c8f188401c9a5252170723383
Helamã Ewerton <38855938+Hecris10@users.noreply.github.com>
2023-03-26 00:31:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,27 +2,28 @@ function soma() {
   var soma1 = document.getElementById("soma1").value;
   console.log(soma1);
   var soma2 = document.getElementById("soma2").value;
-  var resultado = parseInt(soma1) + parseInt(soma2);
+  var resultado = parseFloat(soma1).toString() + parseFloat(soma2);
+  console.log(resultado);
   document.getElementById("soma-resultado").value = resultado;
 }
 
 function subtracao() {
   var sub1 = document.getElementById("subtracao1").value;
   var sub2 = document.getElementById("subtracao2").value;
-  var resultado = parseInt(sub1) - parseInt(sub2);
+  var resultado = parseFloat(sub1) - parseFloat(sub2);
   document.getElementById("subtracao-resultado").value = resultado;
 }
 
 function multiplicacao() {
   var mult1 = document.getElementById("multiplicacao1").value;
   var mult2 = document.getElementById("multiplicacao2").value;
-  var resultado = parseInt(mult1) * parseInt(mult2);
+  var resultado = parseFloat(mult1) * parseFloat(mult2);
   document.getElementById("multiplicacao-resultado").value = resultado;
 }
 
 function divisao() {
   var div1 = document.getElementById("divisao1").value;
   var div2 = document.getElementById("divisao2").value;
-  var resultado = parseInt(div1) / parseInt(div2);
+  var resultado = parseFloat(div1) / parseFloat(div2);
   document.getElementById("divisao-resultado").value = resultado;
 }
